7972fd7f7cedf8b0ec7250cc2cf4de2467139b9c
Walexz01 <adegbiteadewaleoluwaseun@gmail.com>
2024-01-26 00:14:00 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <div className="container">
+          <h1>Coming Soon!</h1>
+        </div>
+      </header>
+    </main>
+  );
+}
